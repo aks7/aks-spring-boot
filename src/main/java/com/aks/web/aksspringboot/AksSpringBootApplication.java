19d236237ca2fc790ee2aa847de5896e40ed373c
aks7 <a4amit6@gmail.com>
2018-04-07 01:48:04 +0530

    first commit

---
+++ src/main/java/com/aks/web/aksspringboot/AksSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.aks.web.aksspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AksSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AksSpringBootApplication.class, args);
+	}
+}
